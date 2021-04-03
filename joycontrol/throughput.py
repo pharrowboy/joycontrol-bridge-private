9aebe5a64749392499ca96be51e0c7d6b3a2ce73
azure <rz7@outlook.jp>
2021-04-03 23:18:10 +0900

    Configurable Interval

--- joycontrol/throughput.py
+++ joycontrol/throughput.py
@@ -7,12 +7,13 @@ class ThroughputMonitor:
 
     INTERVAL = timedelta(seconds=3)
 
-    def __init__(self):
+    def __init__(self, interval=INTERVAL):
         self.start_time = datetime.now()
         self.current_count = 0
         self.last_time = self.start_time
         self.last_count = 0
         self.counts_sec = 0
+        self.interval = interval
 
     def increment(self):
         self.current_count += 1
@@ -20,8 +21,8 @@ class ThroughputMonitor:
     def update(self):
         now = datetime.now()
         count = self.current_count
-        if (now - self.start_time) >= INTERVAL:
-            self.counts_sec = count / INTERVAL.seconds()
+        if (now - self.start_time) >= self.interval:
+            self.counts_sec = count / self.interval.seconds()
             self.start_time = now
             self.current_count = 0
         self.last_time = now
